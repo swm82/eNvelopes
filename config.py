6869c732b97f092a9dc52a5fe7b9545ee71fc4bd
swm82 <swm82@scarletmail.rutgers.edu>
2020-10-09 20:37:07 -0400

    Working app, ready for initial deploy

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    ENV_MAIL_SUBJECT_PREFIX = '[Envelopes]'
+    ENV_MAIL_SENDER = 'Envelopes Admin <EnvelopesTheApp@gmail.com>'
+    ENV_ADMIN = os.environ.get('ENV_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
