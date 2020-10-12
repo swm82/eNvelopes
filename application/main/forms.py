e30fac73c723b2db679d8b3b2305fee74e3375cb
swm82 <swm82@scarletmail.rutgers.edu>
2020-10-12 15:15:01 -0400

    Subclassed DecimalField to allow commas in cash input fields

--- application/main/forms.py
+++ application/main/forms.py
@@ -4,6 +4,12 @@ from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from .. models import Category
 
+class CurrencyField(DecimalField):
+    def process_formdata(self, valuelist):
+        if valuelist:
+            valuelist[0] = valuelist[0].replace(",", "")
+        return super(CurrencyField, self).process_formdata(valuelist)
+
 class AddCategoryForm(FlaskForm):
     category_name = StringField('Category Name:', validators=[DataRequired(), Length(1,64)])
     submit = SubmitField('Add Category')
@@ -12,11 +18,11 @@ class AddTransactionForm(FlaskForm):
     category = SelectField(validators=[DataRequired()])
     category_id = HiddenField()
     payee = StringField(validators=[DataRequired(), Length(1,64)])
-    amount = DecimalField(validators=[DataRequired()], places=2, rounding=None, use_locale=False, number_format=None)
+    amount = CurrencyField(validators=[DataRequired()], places=2, rounding=None, use_locale=False, number_format=None)
     submit = SubmitField('Add Transaction')
 
 class AddToCategoryForm(FlaskForm):
-    amount = DecimalField(validators=[DataRequired()], places=2, rounding=None, use_locale=False, number_format=None)
+    amount = CurrencyField(validators=[DataRequired()], places=2, rounding=None, use_locale=False, number_format=None)
     category_id = HiddenField()
     # to_categories = SelectField('Move to category...')
     to_category_id = HiddenField()
